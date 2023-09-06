a0dffee2d83949653c20162ec2da149b72fc9b80
Newton Ombese <newtonombese1@gmail.com>
2023-09-06 07:22:34 +0300

    Use App.js for testing my components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import HomeNavbar from './components/HomeNavbar';
+import PageNavbar from './components/PageNavbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HomeNavbar />
+      <PageNavbar />
     </div>
   );
 }
