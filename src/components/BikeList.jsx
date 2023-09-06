5886de5f0eaf3da0d27391643d8ee4011922491f
Newton Ombese <newtonombese1@gmail.com>
2023-09-06 11:53:14 +0300

    :recycle: Refactor cleaning up the code

--- src/components/BikeList.jsx
+++ src/components/BikeList.jsx
@@ -5,8 +5,6 @@ import HomeNavbar from './HomeNavbar';
 function BikeList() {
   const state = useSelector((state) => state);
 
-  console.log('first', state);
-
   if (state.bike.isLoading) {
     return <h1>Loading...</h1>;
   }
@@ -17,7 +15,7 @@ function BikeList() {
       <p>BikeList</p>
       <div className="grid gap-2 px-5 grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {
-        state.bike.bikes.networks.map((e) => (
+        state.bike.bikes.networks.slice(0, 28).map((e) => (
           <div className="max-w-sm rounded overflow-hidden shadow-lg" key={e.id}>
             <div className="px-6 py-4">
               <div className="font-bold text-xl mb-2">{e.name}</div>
