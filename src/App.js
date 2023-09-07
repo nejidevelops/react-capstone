1184bf083dfe39616ccb52245c7c44d674911556
Newton Ombese <newtonombese1@gmail.com>
2023-09-07 17:27:24 +0300

    Add routes for the app pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchTeams } from './redux/footballSlice';
+import Teams from './components/Teams';
+import TeamDetails from './components/TeamDetails';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTeams());
+  }, [dispatch]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Teams />} />
+        <Route path="/team/:teamId" element={<TeamDetails />} />
+      </Routes>
     </div>
   );
 }
