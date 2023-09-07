28295fedda42093b296514e86292b44a36a5555d
Newton Ombese <newtonombese1@gmail.com>
2023-09-07 07:06:42 +0300

    Work on my API and fetching

---
+++ src/API/data.js
@@ -0,0 +1,4 @@
+export const URL = 'https://financialmodelingprep.com/api/v3/';
+export const Sectors = 'sectors-performance';
+export const Companies = 'stock-screener';
+export const Company = 'profile/';
