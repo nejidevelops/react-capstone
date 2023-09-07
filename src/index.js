010182b6fd83686025f658fc88ab171b6c9dce5c
Newton Ombese <newtonombese1@gmail.com>
2023-09-07 07:10:47 +0300

    Add index.js file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import store from './redux/store';
-import './index.css';
-import App from './App';
+import Store from './redux/store';
+import './styles/index.css';
+import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider store={Store}>
       <App />
     </Provider>
   </React.StrictMode>,
